f49d457f4980153fd89244354d3598e407d6b84e
Zori <list_babayan@mail.ru>
2022-05-30 11:22:52 +0300

    check_cms bitrix

--- filter.py
+++ filter.py
@@ -4,15 +4,11 @@ from urllib.request import *
 import socket
 import ssl
 import http.client
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 def check_cms(url):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
     page = urlopen(url).read()
-    driver.get(url)
-    driver.set_page_load_timeout(1)
+
     html = BeautifulSoup(page, 'html.parser', from_encoding="iso-8859-1")
 
     metas = html.find_all('link')
@@ -32,10 +28,6 @@ def check_cms(url):
                     fun.close()
 
             break
-            """ if(meta.get('href') == 'B'):
-                p = "совпадение bitrix на сайте "+url
-                fun = open('result.txt', 'a')
-                fun.write(p) """
 
 
 sites = open('list.txt', 'r')
@@ -45,8 +37,8 @@ for site in sites:
     print(site)
 
     try:
-
         check_cms(site)
+
     except HTTPError as e:
         continue
     except URLError as e:
@@ -60,7 +52,5 @@ for site in sites:
     except socket.error as error:
         continue
     except UnicodeEncodeError as e:
-        continue
-    except selenium.common.exceptions.TimeoutException as _e:
         continue
 sites.close()
